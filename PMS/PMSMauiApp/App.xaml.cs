b43c4be352a313b9cd5e184cc148cec56b817218
DeriDeveloper <derideveloper@gmail.com>
2024-03-12 21:48:09 +0300

    Add Web Api Project and maui app

---
+++ PMS/PMSMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PMSMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PMS/PMSMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PMSMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
